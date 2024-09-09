040dadb5da92fc3c485c127f931c12865a901408
Jack Lonergan <99100073+JackTheDevFMD@users.noreply.github.com>
2024-09-09 10:58:16 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,10 +3,10 @@ fx_version 'cerulean'
 game { 'gta5' }
 
 files {
-    'data/**/*.meta'
+    '*.meta'
 }
  
-data_file 'HANDLING_FILE' 'data/**/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
